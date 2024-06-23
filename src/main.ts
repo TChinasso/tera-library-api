4a66f88811267ecea04e962d0791c84c58346a4d
Tchinasso <thiago.chinasso@quiteja.com.br>
2024-06-23 20:44:18 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const port = configService.get('PORT') || 3001;
   app.enableCors({
     origin: '*', // Allow all origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS' // Allow all standard methods
   });
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 3001;
+  
   await app.listen(port);
 }
 bootstrap()

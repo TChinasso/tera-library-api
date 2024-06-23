26e000de212013eba8505800a6be41be2fd24ae3
Tchinasso <thiago.chinasso@quiteja.com.br>
2024-06-23 17:38:04 -0300

    fix(vercel): exporting app to run on vercel infra

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3001);
+  return app
 }
-bootstrap();
+const app = bootstrap();
+
+ export default app

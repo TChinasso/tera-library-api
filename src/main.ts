bbfc0dbc5396c7ec7bc337723b67cbe21c520276
Tchinasso <thiago.chinasso@quiteja.com.br>
2024-06-23 19:09:14 -0300

    fixes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { createServer } from 'http';
+import * as express from 'express';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
-  return app
+  const expressApp = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+  app.listen(3001)
+  await app.init();
+
+  return createServer(expressApp);
 }
 export const maxDuration = 600
 const app = bootstrap();
